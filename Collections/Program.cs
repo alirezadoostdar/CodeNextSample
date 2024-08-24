c4818abed9a96274cddc8fd51325c0393643d62e
alirezadoostdar <alireza.doostdar@gmail.com>
2024-08-24 11:32:03 +0330

    setup collection project

---
+++ Collections/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+public interface IEnumarable
+{
+    IEnumerator GetEnumarator();
+}
+public interface IEnumerator
+{
+    //هر کالکشنی باید این سه متد زیر را داشته باشد
+    bool MoveNext();
+    object Current();
+    void Reset();
+}
+
+public class ListOfInt : IEnumerator
+{
+    int[] _int = [2,3,4,5,6];
+    int _index = -1;
+    public object Current() => _int[_index];
+
+    public bool MoveNext()
+    {
+        if(_index++<_int.Length-1)
+            return true;
+        return false;
+    }
+
+    public void Reset()
+    {
+        _index = -1;
+    }
+}
--- Event/obj/Debug/net8.0/Event.AssemblyInfo.cs
+++ Event/obj/Debug/net8.0/Event.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab6e72bbad7731d75aa645b7e884c00185bf0ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b7be11059559e52f117cc563256c52c14cdca8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
